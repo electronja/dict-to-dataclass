be44af32118dd92062946cc657dd01ff9bdf5a69
Ronja Kamelreiter <ronja.kamelreiter@gmail.com>
2024-08-08 14:30:38 +0200

    Use always latest python-dateutil version

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-python3.8 -m venv venv
+python3 -m venv venv
+
+source ./venv/bin/activate
 
 venv/bin/pip install --upgrade pip
 venv/bin/pip install wheel
-venv/bin/pip install -r requirements.dev.txt
+venv/bin/pip install -r requirements.dev.txt  --upgrade
 
-pre-commit install
